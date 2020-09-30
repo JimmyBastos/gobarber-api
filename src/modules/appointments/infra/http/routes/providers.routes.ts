817e4eb38e3e0591ffcfaf6e43b37f50e204cf7a
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-09-30 16:56:27 -0300

    List day and mont availability routes

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,12 +2,19 @@ import { Router } from 'express'
 
 import isAuthenticated from '@modules/users/infra/http/middlewares/isAuthenticated'
 import ProvidersController from '../controllers/ProvidersController'
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController'
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController'
 
 const providersRouter = Router()
 const providersController = new ProvidersController()
 
+const providerDayAvailabilityController = new ProviderDayAvailabilityController()
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController()
+
 providersRouter.use(isAuthenticated)
 
 providersRouter.get('/', providersController.list)
+providersRouter.get('/:provider_id/day-availability', providerDayAvailabilityController.list)
+providersRouter.get('/:provider_id/month-availability', providerMonthAvailabilityController.list)
 
 export default providersRouter
