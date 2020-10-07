314d5f8b3e920978a12001277baf7b95f472a0f6
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-10-07 20:30:47 -0300

    List provider appointments

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,52 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointmentService'
+
+import ListProviderAppointmentsService from './ListProviderAppointmentsService'
+
+let createAppointment: CreateAppointmentService
+let fakeAppointmentsRepository: FakeAppointmentsRepository
+let listProviderAppointments: ListProviderAppointmentsService
+
+describe('List Provider Appointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository()
+
+    createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    )
+
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository
+    )
+  })
+
+  it('should be able to list appointment hours availability from provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementation(() => {
+      return new Date('2020-01-01 08:00').getTime()
+    })
+
+    const appointmentOne = await createAppointment.execute({
+      provider_id: 'mock-provider-id',
+      customer_id: 'mock-customer-id',
+      date: new Date('2020-01-01 09:00')
+    })
+
+    const appointmentTwo = await createAppointment.execute({
+      provider_id: 'mock-provider-id',
+      customer_id: 'mock-customer-id',
+      date: new Date('2020-01-01 10:00')
+    })
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'mock-provider-id',
+      day: 1,
+      month: 1,
+      year: 2020
+    })
+
+    expect(appointments).toEqual([
+      appointmentOne,
+      appointmentTwo
+    ])
+  })
+})
---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,33 @@
+import { inject, injectable } from 'tsyringe'
+
+import IAppointmentRepository from '../repositories/IAppointmentRepository'
+
+import Appointment from '../infra/typeorm/entities/Appointment'
+
+interface IRequest {
+  provider_id: string,
+  day: number,
+  month: number,
+  year: number
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  constructor (
+     @inject('AppointmentRepository')
+    private appointmentsRepository: IAppointmentRepository
+  ) { }
+
+  public async execute ({ provider_id, day, month, year }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllFromProviderInDay({
+      provider_id,
+      day,
+      month,
+      year
+    })
+
+    return appointments
+  }
+}
+
+export default ListProviderAppointmentsService
