d6d1bcb2ed3114aacb58367a16447047991a505c
Jimmy Bastos <jimmybastosdev@gmail.com>
2020-10-11 21:12:59 -0300

    Add redis config to .env

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -12,10 +12,9 @@ const cacheConfig = {
   driver: 'redis',
   config: {
     redis: {
-      db: 1,
-      host: 'localhost',
-      port: 6379,
-      password: undefined
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD
     }
   }
 } as ICacheConfig
